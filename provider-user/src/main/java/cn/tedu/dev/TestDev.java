fe525f76c160b88a87dcdf627a89bea76de05fc6
yuqun <yuqun6310@163.com>
2021-01-14 10:26:20 +0800

    第一次提交

---
+++ provider-user/src/main/java/cn/tedu/dev/TestDev.java
@@ -0,0 +1,19 @@
+package cn.tedu.dev;
+
+/**
+ * @author yuqun@ihooyah.com
+ * @Description:
+ * @date 2021/1/14 10:22
+ **/
+public class TestDev {
+
+    private String test;
+
+    public String getTest() {
+        return test;
+    }
+
+    public void setTest(String test) {
+        this.test = test;
+    }
+}
